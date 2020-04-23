025f40e868d0b3a081e23e43749984c27b0892d0
Diego Arturo <dart.codes@gmail.com>
2020-04-22 20:46:13 -0400

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+app.use( express.urlencoded({ extended: false }) )
+
+app.get( '/user', ( req, res ) => {
+    res.json('Get User');
+})
+
+app.post( '/user', ( req, res ) => {
+
+    let body = req.body
+
+    res.json({
+        body
+    });
+})
+
+app.put( '/user/:id', ( req, res ) => {
+
+    let id = req.params.id;
+    res.json({
+        id,
+    });
+})
+
+app.delete( '/user', ( req, res ) => {
+    res.json('Delete User');
+})
+
+app.listen( process.env.PORT, () => {
+    console.log(`Listen port ${ process.env.PORT }`);
+})
